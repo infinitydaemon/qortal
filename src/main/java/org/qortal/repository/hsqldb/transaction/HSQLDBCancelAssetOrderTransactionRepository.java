24e5daf51020be0dbe93b8306224f8091db33be9
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 12:20:06 +0600

    Update HSQLDBCancelAssetOrderTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBCancelAssetOrderTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBCancelAssetOrderTransactionRepository.java
@@ -12,39 +12,52 @@ import java.sql.SQLException;
 
 public class HSQLDBCancelAssetOrderTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBCancelAssetOrderTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBCancelAssetOrderTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT asset_order_id FROM CancelAssetOrderTransactions WHERE signature = ?";
+    @Override
+    TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT asset_order_id FROM CancelAssetOrderTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = executeQuery(sql, baseTransactionData.getSignature())) {
+            if (resultSet == null) return null;
 
-			byte[] assetOrderId = resultSet.getBytes(1);
+            byte[] assetOrderId = resultSet.getBytes(1);
 
-			return new CancelAssetOrderTransactionData(baseTransactionData, assetOrderId);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch cancel order transaction from repository", e);
-		}
-	}
+            return new CancelAssetOrderTransactionData(baseTransactionData, assetOrderId);
+        } catch (SQLException e) {
+            throw new DataException("Unable to fetch cancel asset order transaction from repository", e);
+        }
+    }
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		CancelAssetOrderTransactionData cancelOrderTransactionData = (CancelAssetOrderTransactionData) transactionData;
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        CancelAssetOrderTransactionData cancelOrderTransactionData = (CancelAssetOrderTransactionData) transactionData;
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("CancelAssetOrderTransactions");
+        HSQLDBSaver saveHelper = new HSQLDBSaver("CancelAssetOrderTransactions")
+                .bind("signature", cancelOrderTransactionData.getSignature())
+                .bind("creator", cancelOrderTransactionData.getCreatorPublicKey())
+                .bind("asset_order_id", cancelOrderTransactionData.getOrderId());
 
-		saveHelper.bind("signature", cancelOrderTransactionData.getSignature()).bind("creator", cancelOrderTransactionData.getCreatorPublicKey())
-				.bind("asset_order_id", cancelOrderTransactionData.getOrderId());
+        executeSave(saveHelper);
+    }
 
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save cancel order transaction into repository", e);
-		}
-	}
+    // Helper method to execute SQL queries
+    private ResultSet executeQuery(String sql, Object... params) throws DataException {
+        try {
+            return repository.checkedExecute(sql, params);
+        } catch (SQLException e) {
+            throw new DataException("Error executing query", e);
+        }
+    }
 
+    // Helper method to execute save operations
+    private void executeSave(HSQLDBSaver saveHelper) throws DataException {
+        try {
+            saveHelper.execute(repository);
+        } catch (SQLException e) {
+            throw new DataException("Error saving transaction into repository", e);
+        }
+    }
 }
