5b7978ecfd9058abb3def0a5d510daa455a29d04
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 15:05:00 +0600

    Update HSQLDBDeployAtTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBDeployAtTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBDeployAtTransactionRepository.java
@@ -12,51 +12,56 @@ import java.sql.SQLException;
 
 public class HSQLDBDeployAtTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBDeployAtTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBDeployAtTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT AT_name, description, AT_type, AT_tags, creation_bytes, amount, asset_id, AT_address FROM DeployATTransactions WHERE signature = ?";
+    @Override
+    public TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT AT_name, description, AT_type, AT_tags, creation_bytes, amount, asset_id, AT_address "
+                   + "FROM DeployATTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
+            if (resultSet == null || !resultSet.next()) {
+                return null;  // No result, return null
+            }
 
-			String name = resultSet.getString(1);
-			String description = resultSet.getString(2);
-			String atType = resultSet.getString(3);
-			String tags = resultSet.getString(4);
-			byte[] creationBytes = resultSet.getBytes(5);
-			long amount = resultSet.getLong(6);
-			long assetId = resultSet.getLong(7);
+            String name = resultSet.getString("AT_name");
+            String description = resultSet.getString("description");
+            String atType = resultSet.getString("AT_type");
+            String tags = resultSet.getString("AT_tags");
+            byte[] creationBytes = resultSet.getBytes("creation_bytes");
+            long amount = resultSet.getLong("amount");
+            long assetId = resultSet.getLong("asset_id");
 
-			// Special null-checking for AT address
-			String atAddress = resultSet.getString(8);
+            // Handling potential null AT_address
+            String atAddress = resultSet.getString("AT_address");
 
-			return new DeployAtTransactionData(baseTransactionData, atAddress, name, description, atType, tags, creationBytes, amount, assetId);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch deploy AT transaction from repository", e);
-		}
-	}
+            return new DeployAtTransactionData(baseTransactionData, atAddress, name, description, atType, tags, creationBytes, amount, assetId);
+        } catch (SQLException e) {
+            throw new DataException("Unable to fetch deploy AT transaction from repository", e);
+        }
+    }
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		DeployAtTransactionData deployATTransactionData = (DeployAtTransactionData) transactionData;
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        DeployAtTransactionData deployATTransactionData = (DeployAtTransactionData) transactionData;
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("DeployATTransactions");
+        try (HSQLDBSaver saveHelper = new HSQLDBSaver("DeployATTransactions")) {
+            saveHelper.bind("signature", deployATTransactionData.getSignature())
+                      .bind("creator", deployATTransactionData.getCreatorPublicKey())
+                      .bind("AT_name", deployATTransactionData.getName())
+                      .bind("description", deployATTransactionData.getDescription())
+                      .bind("AT_type", deployATTransactionData.getAtType())
+                      .bind("AT_tags", deployATTransactionData.getTags())
+                      .bind("creation_bytes", deployATTransactionData.getCreationBytes())
+                      .bind("amount", deployATTransactionData.getAmount())
+                      .bind("asset_id", deployATTransactionData.getAssetId())
+                      .bind("AT_address", deployATTransactionData.getAtAddress());
 
-		saveHelper.bind("signature", deployATTransactionData.getSignature()).bind("creator", deployATTransactionData.getCreatorPublicKey())
-				.bind("AT_name", deployATTransactionData.getName()).bind("description", deployATTransactionData.getDescription())
-				.bind("AT_type", deployATTransactionData.getAtType()).bind("AT_tags", deployATTransactionData.getTags())
-				.bind("creation_bytes", deployATTransactionData.getCreationBytes()).bind("amount", deployATTransactionData.getAmount())
-				.bind("asset_id", deployATTransactionData.getAssetId()).bind("AT_address", deployATTransactionData.getAtAddress());
-
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save deploy AT transaction into repository", e);
-		}
-	}
-
+            saveHelper.execute(this.repository);
+        } catch (SQLException e) {
+            throw new DataException("Unable to save deploy AT transaction into repository", e);
+        }
+    }
 }
