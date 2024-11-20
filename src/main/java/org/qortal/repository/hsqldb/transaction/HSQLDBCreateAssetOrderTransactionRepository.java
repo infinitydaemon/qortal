641bab7243b24d22f95a537453236a6d20da75fc
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 15:02:58 +0600

    Update HSQLDBCreateAssetOrderTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBCreateAssetOrderTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBCreateAssetOrderTransactionRepository.java
@@ -12,50 +12,51 @@ import java.sql.SQLException;
 
 public class HSQLDBCreateAssetOrderTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBCreateAssetOrderTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBCreateAssetOrderTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		// LEFT OUTER JOIN because asset might not exist (e.g. if ISSUE_ASSET & CREATE_ASSET_ORDER are both unconfirmed)
-		String sql = "SELECT have_asset_id, amount, want_asset_id, price, HaveAsset.asset_name, WantAsset.asset_name "
-				+ "FROM CreateAssetOrderTransactions "
-				+ "LEFT OUTER JOIN Assets AS HaveAsset ON HaveAsset.asset_id = have_asset_id "
-				+ "LEFT OUTER JOIN Assets AS WantAsset ON WantAsset.asset_id = want_asset_id "
-				+ "WHERE signature = ?";
+    @Override
+    public TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        // LEFT OUTER JOIN because asset might not exist (e.g. if ISSUE_ASSET & CREATE_ASSET_ORDER are both unconfirmed)
+        String sql = "SELECT have_asset_id, amount, want_asset_id, price, HaveAsset.asset_name, WantAsset.asset_name "
+                   + "FROM CreateAssetOrderTransactions "
+                   + "LEFT OUTER JOIN Assets AS HaveAsset ON HaveAsset.asset_id = have_asset_id "
+                   + "LEFT OUTER JOIN Assets AS WantAsset ON WantAsset.asset_id = want_asset_id "
+                   + "WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
+            if (resultSet != null && resultSet.next()) { // Ensure a valid row exists
+                long haveAssetId = resultSet.getLong("have_asset_id");
+                long amount = resultSet.getLong("amount");
+                long wantAssetId = resultSet.getLong("want_asset_id");
+                long price = resultSet.getLong("price");
+                String haveAssetName = resultSet.getString("HaveAsset.asset_name");
+                String wantAssetName = resultSet.getString("WantAsset.asset_name");
 
-			long haveAssetId = resultSet.getLong(1);
-			long amount = resultSet.getLong(2);
-			long wantAssetId = resultSet.getLong(3);
-			long price = resultSet.getLong(4);
-			String haveAssetName = resultSet.getString(5);
-			String wantAssetName = resultSet.getString(6);
+                return new CreateAssetOrderTransactionData(baseTransactionData, haveAssetId, wantAssetId, amount, price, haveAssetName, wantAssetName);
+            }
+            return null; // Explicitly return null if no data found
+        } catch (SQLException e) {
+            throw new DataException("Unable to fetch create order transaction from repository", e);
+        }
+    }
 
-			return new CreateAssetOrderTransactionData(baseTransactionData, haveAssetId, wantAssetId, amount, price, haveAssetName, wantAssetName);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch create order transaction from repository", e);
-		}
-	}
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        CreateAssetOrderTransactionData createOrderTransactionData = (CreateAssetOrderTransactionData) transactionData;
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		CreateAssetOrderTransactionData createOrderTransactionData = (CreateAssetOrderTransactionData) transactionData;
+        try (HSQLDBSaver saveHelper = new HSQLDBSaver("CreateAssetOrderTransactions")) {
+            saveHelper.bind("signature", createOrderTransactionData.getSignature())
+                    .bind("creator", createOrderTransactionData.getCreatorPublicKey())
+                    .bind("have_asset_id", createOrderTransactionData.getHaveAssetId())
+                    .bind("amount", createOrderTransactionData.getAmount())
+                    .bind("want_asset_id", createOrderTransactionData.getWantAssetId())
+                    .bind("price", createOrderTransactionData.getPrice());
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("CreateAssetOrderTransactions");
-
-		saveHelper.bind("signature", createOrderTransactionData.getSignature()).bind("creator", createOrderTransactionData.getCreatorPublicKey())
-				.bind("have_asset_id", createOrderTransactionData.getHaveAssetId()).bind("amount", createOrderTransactionData.getAmount())
-				.bind("want_asset_id", createOrderTransactionData.getWantAssetId()).bind("price", createOrderTransactionData.getPrice());
-
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save create order transaction into repository", e);
-		}
-	}
-
+            saveHelper.execute(this.repository);
+        } catch (SQLException e) {
+            throw new DataException("Unable to save create order transaction into repository", e);
+        }
+    }
 }
