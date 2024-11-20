d03ad4f75d2fcdd9ad56656ec25b492ac9e7a337
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 15:09:43 +0600

    Update HSQLDBSetGroupTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBSetGroupTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBSetGroupTransactionRepository.java
@@ -12,42 +12,43 @@ import java.sql.SQLException;
 
 public class HSQLDBSetGroupTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBSetGroupTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBSetGroupTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT default_group_id, previous_default_group_id FROM SetGroupTransactions WHERE signature = ?";
+    @Override
+    public TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT default_group_id, previous_default_group_id FROM SetGroupTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
+            if (resultSet == null || !resultSet.next()) {
+                return null;  // Return null if no result is found
+            }
 
-			int defaultGroupId = resultSet.getInt(1);
-			Integer previousDefaultGroupId = resultSet.getInt(2);
-			if (previousDefaultGroupId == 0 && resultSet.wasNull())
-				previousDefaultGroupId = null;
+            int defaultGroupId = resultSet.getInt("default_group_id");
+            Integer previousDefaultGroupId = resultSet.getInt("previous_default_group_id");
+            if (previousDefaultGroupId == 0 && resultSet.wasNull()) {
+                previousDefaultGroupId = null;
+            }
 
-			return new SetGroupTransactionData(baseTransactionData, defaultGroupId, previousDefaultGroupId);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch set group transaction from repository", e);
-		}
-	}
+            return new SetGroupTransactionData(baseTransactionData, defaultGroupId, previousDefaultGroupId);
+        } catch (SQLException e) {
+            throw new DataException("Unable to fetch set group transaction from repository", e);
+        }
+    }
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		SetGroupTransactionData setGroupTransactionData = (SetGroupTransactionData) transactionData;
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        SetGroupTransactionData setGroupTransactionData = (SetGroupTransactionData) transactionData;
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("SetGroupTransactions");
+        try (HSQLDBSaver saveHelper = new HSQLDBSaver("SetGroupTransactions")) {
+            saveHelper.bind("signature", setGroupTransactionData.getSignature())
+                      .bind("default_group_id", setGroupTransactionData.getDefaultGroupId())
+                      .bind("previous_default_group_id", setGroupTransactionData.getPreviousDefaultGroupId());
 
-		saveHelper.bind("signature", setGroupTransactionData.getSignature()).bind("default_group_id", setGroupTransactionData.getDefaultGroupId())
-				.bind("previous_default_group_id", setGroupTransactionData.getPreviousDefaultGroupId());
-
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save set group transaction into repository", e);
-		}
-	}
-
+            saveHelper.execute(this.repository);
+        } catch (SQLException e) {
+            throw new DataException("Unable to save set group transaction into repository", e);
+        }
+    }
 }
