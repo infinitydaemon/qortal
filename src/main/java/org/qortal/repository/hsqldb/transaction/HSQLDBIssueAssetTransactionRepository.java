8bb296ec0081ecce61e08e8b76c004338887c603
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 12:26:24 +0600

    Update HSQLDBIssueAssetTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBIssueAssetTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBIssueAssetTransactionRepository.java
@@ -12,56 +12,72 @@ import java.sql.SQLException;
 
 public class HSQLDBIssueAssetTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBIssueAssetTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBIssueAssetTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT asset_name, description, quantity, is_divisible, data, is_unspendable, asset_id, reduced_asset_name "
-				+ "FROM IssueAssetTransactions WHERE signature = ?";
+    @Override
+    TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT asset_name, description, quantity, is_divisible, data, is_unspendable, asset_id, reduced_asset_name "
+                + "FROM IssueAssetTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = executeQuery(sql, baseTransactionData.getSignature())) {
+            if (resultSet == null) return null;
 
-			String assetName = resultSet.getString(1);
-			String description = resultSet.getString(2);
-			long quantity = resultSet.getLong(3);
-			boolean isDivisible = resultSet.getBoolean(4);
-			String data = resultSet.getString(5);
-			boolean isUnspendable = resultSet.getBoolean(6);
+            String assetName = resultSet.getString(1);
+            String description = resultSet.getString(2);
+            long quantity = resultSet.getLong(3);
+            boolean isDivisible = resultSet.getBoolean(4);
+            String data = resultSet.getString(5);
+            boolean isUnspendable = resultSet.getBoolean(6);
 
-			// Special null-checking for asset ID
-			Long assetId = resultSet.getLong(7);
-			if (assetId == 0 && resultSet.wasNull())
-				assetId = null;
+            // Special null-checking for asset ID
+            Long assetId = resultSet.getLong(7);
+            if (assetId == 0 && resultSet.wasNull()) assetId = null;
 
-			String reducedAssetName = resultSet.getString(8);
+            String reducedAssetName = resultSet.getString(8);
 
-			return new IssueAssetTransactionData(baseTransactionData, assetId, assetName, description, quantity, isDivisible,
-					data, isUnspendable, reducedAssetName);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch issue asset transaction from repository", e);
-		}
-	}
+            return new IssueAssetTransactionData(baseTransactionData, assetId, assetName, description, quantity, isDivisible,
+                    data, isUnspendable, reducedAssetName);
+        } catch (SQLException e) {
+            throw new DataException("Unable to fetch issue asset transaction from repository", e);
+        }
+    }
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		IssueAssetTransactionData issueAssetTransactionData = (IssueAssetTransactionData) transactionData;
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        IssueAssetTransactionData issueAssetTransactionData = (IssueAssetTransactionData) transactionData;
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("IssueAssetTransactions");
+        HSQLDBSaver saveHelper = new HSQLDBSaver("IssueAssetTransactions")
+                .bind("signature", issueAssetTransactionData.getSignature())
+                .bind("issuer", issueAssetTransactionData.getIssuerPublicKey())
+                .bind("asset_name", issueAssetTransactionData.getAssetName())
+                .bind("reduced_asset_name", issueAssetTransactionData.getReducedAssetName())
+                .bind("description", issueAssetTransactionData.getDescription())
+                .bind("quantity", issueAssetTransactionData.getQuantity())
+                .bind("is_divisible", issueAssetTransactionData.isDivisible())
+                .bind("data", issueAssetTransactionData.getData())
+                .bind("is_unspendable", issueAssetTransactionData.isUnspendable())
+                .bind("asset_id", issueAssetTransactionData.getAssetId());
 
-		saveHelper.bind("signature", issueAssetTransactionData.getSignature()).bind("issuer", issueAssetTransactionData.getIssuerPublicKey())
-				.bind("asset_name", issueAssetTransactionData.getAssetName()).bind("reduced_asset_name", issueAssetTransactionData.getReducedAssetName())
-				.bind("description", issueAssetTransactionData.getDescription()).bind("quantity", issueAssetTransactionData.getQuantity())
-				.bind("is_divisible", issueAssetTransactionData.isDivisible()).bind("data", issueAssetTransactionData.getData())
-				.bind("is_unspendable", issueAssetTransactionData.isUnspendable()).bind("asset_id", issueAssetTransactionData.getAssetId());
+        executeSave(saveHelper);
+    }
 
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save issue asset transaction into repository", e);
-		}
-	}
+    // Helper method to execute SQL queries
+    private ResultSet executeQuery(String sql, Object... params) throws DataException {
+        try {
+            return repository.checkedExecute(sql, params);
+        } catch (SQLException e) {
+            throw new DataException("Error executing query", e);
+        }
+    }
 
+    // Helper method to execute save operations
+    private void executeSave(HSQLDBSaver saveHelper) throws DataException {
+        try {
+            saveHelper.execute(repository);
+        } catch (SQLException e) {
+            throw new DataException("Error saving transaction into repository", e);
+        }
+    }
 }
