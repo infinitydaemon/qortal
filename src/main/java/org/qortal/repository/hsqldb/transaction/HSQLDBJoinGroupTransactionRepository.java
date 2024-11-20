2bdf7c91bfc70f62da0c614c2876b7620f492108
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 15:08:59 +0600

    Update HSQLDBJoinGroupTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBJoinGroupTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBJoinGroupTransactionRepository.java
@@ -12,45 +12,47 @@ import java.sql.SQLException;
 
 public class HSQLDBJoinGroupTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBJoinGroupTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBJoinGroupTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT group_id, invite_reference, previous_group_id FROM JoinGroupTransactions WHERE signature = ?";
+    @Override
+    public TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT group_id, invite_reference, previous_group_id FROM JoinGroupTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
+            if (resultSet == null || !resultSet.next()) {
+                return null;  // Return null if no result is found
+            }
 
-			int groupId = resultSet.getInt(1);
-			byte[] inviteReference = resultSet.getBytes(2);
+            int groupId = resultSet.getInt("group_id");
+            byte[] inviteReference = resultSet.getBytes("invite_reference");
 
-			Integer previousGroupId = resultSet.getInt(3);
-			if (previousGroupId == 0 && resultSet.wasNull())
-				previousGroupId = null;
+            Integer previousGroupId = resultSet.getInt("previous_group_id");
+            if (previousGroupId == 0 && resultSet.wasNull()) {
+                previousGroupId = null;
+            }
 
-			return new JoinGroupTransactionData(baseTransactionData, groupId, inviteReference, previousGroupId);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch join group transaction from repository", e);
-		}
-	}
+            return new JoinGroupTransactionData(baseTransactionData, groupId, inviteReference, previousGroupId);
+        } catch (SQLException e) {
+            throw new DataException("Unable to fetch join group transaction from repository", e);
+        }
+    }
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		JoinGroupTransactionData joinGroupTransactionData = (JoinGroupTransactionData) transactionData;
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        JoinGroupTransactionData joinGroupTransactionData = (JoinGroupTransactionData) transactionData;
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("JoinGroupTransactions");
+        try (HSQLDBSaver saveHelper = new HSQLDBSaver("JoinGroupTransactions")) {
+            saveHelper.bind("signature", joinGroupTransactionData.getSignature())
+                      .bind("joiner", joinGroupTransactionData.getJoinerPublicKey())
+                      .bind("group_id", joinGroupTransactionData.getGroupId())
+                      .bind("invite_reference", joinGroupTransactionData.getInviteReference())
+                      .bind("previous_group_id", joinGroupTransactionData.getPreviousGroupId());
 
-		saveHelper.bind("signature", joinGroupTransactionData.getSignature()).bind("joiner", joinGroupTransactionData.getJoinerPublicKey())
-				.bind("group_id", joinGroupTransactionData.getGroupId()).bind("invite_reference", joinGroupTransactionData.getInviteReference())
-				.bind("previous_group_id", joinGroupTransactionData.getPreviousGroupId());
-
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save join group transaction into repository", e);
-		}
-	}
-
+            saveHelper.execute(this.repository);
+        } catch (SQLException e) {
+            throw new DataException("Unable to save join group transaction into repository", e);
+        }
+    }
 }
