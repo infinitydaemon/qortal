e63cc5213c6d7e007c0aca35bf352406821bb710
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 15:07:26 +0600

    Update HSQLDBGroupInviteTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBGroupInviteTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBGroupInviteTransactionRepository.java
@@ -12,48 +12,52 @@ import java.sql.SQLException;
 
 public class HSQLDBGroupInviteTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBGroupInviteTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBGroupInviteTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT group_id, invitee, time_to_live, join_reference, previous_group_id FROM GroupInviteTransactions WHERE signature = ?";
+    @Override
+    public TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT group_id, invitee, time_to_live, join_reference, previous_group_id " +
+                     "FROM GroupInviteTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
+            if (resultSet == null || !resultSet.next()) {
+                return null;  // Return null if no result is found
+            }
 
-			int groupId = resultSet.getInt(1);
-			String invitee = resultSet.getString(2);
-			int timeToLive = resultSet.getInt(3);
-			byte[] joinReference = resultSet.getBytes(4);
+            int groupId = resultSet.getInt("group_id");
+            String invitee = resultSet.getString("invitee");
+            int timeToLive = resultSet.getInt("time_to_live");
+            byte[] joinReference = resultSet.getBytes("join_reference");
 
-			Integer previousGroupId = resultSet.getInt(5);
-			if (previousGroupId == 0 && resultSet.wasNull())
-				previousGroupId = null;
+            Integer previousGroupId = resultSet.getInt("previous_group_id");
+            if (previousGroupId == 0 && resultSet.wasNull()) {
+                previousGroupId = null;
+            }
 
-			return new GroupInviteTransactionData(baseTransactionData, groupId, invitee, timeToLive, joinReference, previousGroupId);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch group invite transaction from repository", e);
-		}
-	}
+            return new GroupInviteTransactionData(baseTransactionData, groupId, invitee, timeToLive, joinReference, previousGroupId);
+        } catch (SQLException e) {
+            throw new DataException("Unable to fetch group invite transaction from repository", e);
+        }
+    }
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		GroupInviteTransactionData groupInviteTransactionData = (GroupInviteTransactionData) transactionData;
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        GroupInviteTransactionData groupInviteTransactionData = (GroupInviteTransactionData) transactionData;
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("GroupInviteTransactions");
+        try (HSQLDBSaver saveHelper = new HSQLDBSaver("GroupInviteTransactions")) {
+            saveHelper.bind("signature", groupInviteTransactionData.getSignature())
+                      .bind("admin", groupInviteTransactionData.getAdminPublicKey())
+                      .bind("group_id", groupInviteTransactionData.getGroupId())
+                      .bind("invitee", groupInviteTransactionData.getInvitee())
+                      .bind("time_to_live", groupInviteTransactionData.getTimeToLive())
+                      .bind("join_reference", groupInviteTransactionData.getJoinReference())
+                      .bind("previous_group_id", groupInviteTransactionData.getPreviousGroupId());
 
-		saveHelper.bind("signature", groupInviteTransactionData.getSignature()).bind("admin", groupInviteTransactionData.getAdminPublicKey())
-				.bind("group_id", groupInviteTransactionData.getGroupId()).bind("invitee", groupInviteTransactionData.getInvitee())
-				.bind("time_to_live", groupInviteTransactionData.getTimeToLive()).bind("join_reference", groupInviteTransactionData.getJoinReference())
-				.bind("previous_group_id", groupInviteTransactionData.getPreviousGroupId());
-
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save group invite transaction into repository", e);
-		}
-	}
-
+            saveHelper.execute(this.repository);
+        } catch (SQLException e) {
+            throw new DataException("Unable to save group invite transaction into repository", e);
+        }
+    }
 }
