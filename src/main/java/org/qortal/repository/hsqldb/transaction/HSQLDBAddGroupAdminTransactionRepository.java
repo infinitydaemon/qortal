b6eeff1574fe2541960d14a9f0864a599b0d4410
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 12:17:12 +0600

    Update HSQLDBAddGroupAdminTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBAddGroupAdminTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBAddGroupAdminTransactionRepository.java
@@ -12,40 +12,52 @@ import java.sql.SQLException;
 
 public class HSQLDBAddGroupAdminTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBAddGroupAdminTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBAddGroupAdminTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT group_id, address FROM AddGroupAdminTransactions WHERE signature = ?";
+    @Override
+    TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT group_id, address FROM AddGroupAdminTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = executeQuery(sql, baseTransactionData.getSignature())) {
+            if (resultSet == null) return null;
 
-			int groupId = resultSet.getInt(1);
-			String member = resultSet.getString(2);
+            int groupId = resultSet.getInt(1);
+            String member = resultSet.getString(2);
 
-			return new AddGroupAdminTransactionData(baseTransactionData, groupId, member);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch add group admin transaction from repository", e);
-		}
-	}
+            return new AddGroupAdminTransactionData(baseTransactionData, groupId, member);
+        } catch (SQLException e) {
+            throw new DataException("Error fetching add group admin transaction from repository", e);
+        }
+    }
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		AddGroupAdminTransactionData addGroupAdminTransactionData = (AddGroupAdminTransactionData) transactionData;
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        AddGroupAdminTransactionData addGroupAdminTransactionData = (AddGroupAdminTransactionData) transactionData;
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("AddGroupAdminTransactions");
+        HSQLDBSaver saveHelper = new HSQLDBSaver("AddGroupAdminTransactions")
+                .bind("signature", addGroupAdminTransactionData.getSignature())
+                .bind("owner", addGroupAdminTransactionData.getOwnerPublicKey())
+                .bind("group_id", addGroupAdminTransactionData.getGroupId())
+                .bind("address", addGroupAdminTransactionData.getMember());
 
-		saveHelper.bind("signature", addGroupAdminTransactionData.getSignature()).bind("owner", addGroupAdminTransactionData.getOwnerPublicKey())
-				.bind("group_id", addGroupAdminTransactionData.getGroupId()).bind("address", addGroupAdminTransactionData.getMember());
+        executeSave(saveHelper);
+    }
 
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save add group admin transaction into repository", e);
-		}
-	}
+    private ResultSet executeQuery(String sql, Object... params) throws DataException {
+        try {
+            return this.repository.checkedExecute(sql, params);
+        } catch (SQLException e) {
+            throw new DataException("Error executing query", e);
+        }
+    }
 
+    private void executeSave(HSQLDBSaver saveHelper) throws DataException {
+        try {
+            saveHelper.execute(this.repository);
+        } catch (SQLException e) {
+            throw new DataException("Error saving transaction into repository", e);
+        }
+    }
 }
