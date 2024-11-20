163ee9f53e6374376ac0587b668e1a9619af09a9
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 12:27:23 +0600

    Update HSQLDBLeaveGroupTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBLeaveGroupTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBLeaveGroupTransactionRepository.java
@@ -12,46 +12,60 @@ import java.sql.SQLException;
 
 public class HSQLDBLeaveGroupTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBLeaveGroupTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBLeaveGroupTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT group_id, member_reference, admin_reference, previous_group_id FROM LeaveGroupTransactions WHERE signature = ?";
+    @Override
+    TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT group_id, member_reference, admin_reference, previous_group_id FROM LeaveGroupTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = executeQuery(sql, baseTransactionData.getSignature())) {
+            if (resultSet == null) return null;
 
-			int groupId = resultSet.getInt(1);
-			byte[] memberReference = resultSet.getBytes(2);
-			byte[] adminReference = resultSet.getBytes(3);
+            int groupId = resultSet.getInt(1);
+            byte[] memberReference = resultSet.getBytes(2);
+            byte[] adminReference = resultSet.getBytes(3);
 
-			Integer previousGroupId = resultSet.getInt(4);
-			if (previousGroupId == 0 && resultSet.wasNull())
-				previousGroupId = null;
+            Integer previousGroupId = resultSet.getInt(4);
+            if (previousGroupId == 0 && resultSet.wasNull()) previousGroupId = null;
 
-			return new LeaveGroupTransactionData(baseTransactionData, groupId, memberReference, adminReference, previousGroupId);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch leave group transaction from repository", e);
-		}
-	}
+            return new LeaveGroupTransactionData(baseTransactionData, groupId, memberReference, adminReference, previousGroupId);
+        } catch (SQLException e) {
+            throw new DataException("Unable to fetch leave group transaction from repository", e);
+        }
+    }
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		LeaveGroupTransactionData leaveGroupTransactionData = (LeaveGroupTransactionData) transactionData;
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        LeaveGroupTransactionData leaveGroupTransactionData = (LeaveGroupTransactionData) transactionData;
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("LeaveGroupTransactions");
+        HSQLDBSaver saveHelper = new HSQLDBSaver("LeaveGroupTransactions")
+                .bind("signature", leaveGroupTransactionData.getSignature())
+                .bind("leaver", leaveGroupTransactionData.getLeaverPublicKey())
+                .bind("group_id", leaveGroupTransactionData.getGroupId())
+                .bind("member_reference", leaveGroupTransactionData.getMemberReference())
+                .bind("admin_reference", leaveGroupTransactionData.getAdminReference())
+                .bind("previous_group_id", leaveGroupTransactionData.getPreviousGroupId());
 
-		saveHelper.bind("signature", leaveGroupTransactionData.getSignature()).bind("leaver", leaveGroupTransactionData.getLeaverPublicKey())
-				.bind("group_id", leaveGroupTransactionData.getGroupId()).bind("member_reference", leaveGroupTransactionData.getMemberReference())
-				.bind("admin_reference", leaveGroupTransactionData.getAdminReference()).bind("previous_group_id", leaveGroupTransactionData.getPreviousGroupId());
+        executeSave(saveHelper);
+    }
 
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save leave group transaction into repository", e);
-		}
-	}
+    // Helper method to execute SQL queries
+    private ResultSet executeQuery(String sql, Object... params) throws DataException {
+        try {
+            return repository.checkedExecute(sql, params);
+        } catch (SQLException e) {
+            throw new DataException("Error executing query", e);
+        }
+    }
 
+    // Helper method to execute save operations
+    private void executeSave(HSQLDBSaver saveHelper) throws DataException {
+        try {
+            saveHelper.execute(repository);
+        } catch (SQLException e) {
+            throw new DataException("Error saving transaction into repository", e);
+        }
+    }
 }
