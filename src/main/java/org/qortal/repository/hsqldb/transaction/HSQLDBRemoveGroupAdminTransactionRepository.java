0fc0c424e946086d379df77387878c61439c116c
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 15:13:27 +0600

    Update HSQLDBRemoveGroupAdminTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBRemoveGroupAdminTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBRemoveGroupAdminTransactionRepository.java
@@ -12,42 +12,48 @@ import java.sql.SQLException;
 
 public class HSQLDBRemoveGroupAdminTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBRemoveGroupAdminTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBRemoveGroupAdminTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT group_id, admin, admin_reference FROM RemoveGroupAdminTransactions WHERE signature = ?";
+    @Override
+    public TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT group_id, admin, admin_reference FROM RemoveGroupAdminTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
+            // If no record is found, return null
+            if (!resultSet.next()) {
+                return null;
+            }
 
-			int groupId = resultSet.getInt(1);
-			String admin = resultSet.getString(2);
-			byte[] adminReference = resultSet.getBytes(3);
+            // Retrieve values from the result set
+            int groupId = resultSet.getInt("group_id");
+            String admin = resultSet.getString("admin");
+            byte[] adminReference = resultSet.getBytes("admin_reference");
 
-			return new RemoveGroupAdminTransactionData(baseTransactionData, groupId, admin, adminReference);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch remove group admin transaction from repository", e);
-		}
-	}
+            // Return the RemoveGroupAdminTransactionData instance with the retrieved values
+            return new RemoveGroupAdminTransactionData(baseTransactionData, groupId, admin, adminReference);
+        } catch (SQLException e) {
+            throw new DataException("Unable to fetch remove group admin transaction from repository", e);
+        }
+    }
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		RemoveGroupAdminTransactionData removeGroupAdminTransactionData = (RemoveGroupAdminTransactionData) transactionData;
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        RemoveGroupAdminTransactionData removeGroupAdminTransactionData = (RemoveGroupAdminTransactionData) transactionData;
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("RemoveGroupAdminTransactions");
+        try (HSQLDBSaver saveHelper = new HSQLDBSaver("RemoveGroupAdminTransactions")) {
+            // Bind values to the SQL query
+            saveHelper.bind("signature", removeGroupAdminTransactionData.getSignature())
+                      .bind("owner", removeGroupAdminTransactionData.getOwnerPublicKey())
+                      .bind("group_id", removeGroupAdminTransactionData.getGroupId())
+                      .bind("admin", removeGroupAdminTransactionData.getAdmin())
+                      .bind("admin_reference", removeGroupAdminTransactionData.getAdminReference());
 
-		saveHelper.bind("signature", removeGroupAdminTransactionData.getSignature()).bind("owner", removeGroupAdminTransactionData.getOwnerPublicKey())
-				.bind("group_id", removeGroupAdminTransactionData.getGroupId()).bind("admin", removeGroupAdminTransactionData.getAdmin())
-				.bind("admin_reference", removeGroupAdminTransactionData.getAdminReference());
-
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save remove group admin transaction into repository", e);
-		}
-	}
-
+            // Execute the insert operation
+            saveHelper.execute(this.repository);
+        } catch (SQLException e) {
+            throw new DataException("Unable to save remove group admin transaction into repository", e);
+        }
+    }
 }
