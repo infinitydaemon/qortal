7f65a477be91937af87dbb20579c33be7a0a0c63
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 12:16:26 +0600

    Update HSQLDBAccountLevelTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBAccountLevelTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBAccountLevelTransactionRepository.java
@@ -12,40 +12,54 @@ import java.sql.SQLException;
 
 public class HSQLDBAccountLevelTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBAccountLevelTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBAccountLevelTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT target, level FROM AccountLevelTransactions WHERE signature = ?";
+    @Override
+    TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT target, level FROM AccountLevelTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = executeQuery(sql, baseTransactionData.getSignature())) {
+            if (resultSet == null) return null;
 
-			String target = resultSet.getString(1);
-			int level = resultSet.getInt(2);
+            String target = resultSet.getString(1);
+            int level = resultSet.getInt(2);
 
-			return new AccountLevelTransactionData(baseTransactionData, target, level);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch account level transaction from repository", e);
-		}
-	}
+            return new AccountLevelTransactionData(baseTransactionData, target, level);
+        } catch (SQLException e) {
+            throw new DataException("Error fetching account level transaction from repository", e);
+        }
+    }
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		AccountLevelTransactionData accountLevelTransactionData = (AccountLevelTransactionData) transactionData;
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        AccountLevelTransactionData accountLevelTransactionData = (AccountLevelTransactionData) transactionData;
+        
+        String tableName = "AccountLevelTransactions";
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("AccountLevelTransactions");
+        HSQLDBSaver saveHelper = new HSQLDBSaver(tableName)
+                .bind("signature", accountLevelTransactionData.getSignature())
+                .bind("creator", accountLevelTransactionData.getCreatorPublicKey())
+                .bind("target", accountLevelTransactionData.getTarget())
+                .bind("level", accountLevelTransactionData.getLevel());
 
-		saveHelper.bind("signature", accountLevelTransactionData.getSignature()).bind("creator", accountLevelTransactionData.getCreatorPublicKey())
-				.bind("target", accountLevelTransactionData.getTarget()).bind("level", accountLevelTransactionData.getLevel());
+        executeSave(saveHelper);
+    }
 
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save account level transaction into repository", e);
-		}
-	}
+    private ResultSet executeQuery(String sql, Object... params) throws DataException {
+        try {
+            return this.repository.checkedExecute(sql, params);
+        } catch (SQLException e) {
+            throw new DataException("Error executing query", e);
+        }
+    }
 
+    private void executeSave(HSQLDBSaver saveHelper) throws DataException {
+        try {
+            saveHelper.execute(this.repository);
+        } catch (SQLException e) {
+            throw new DataException("Error saving transaction into repository", e);
+        }
+    }
 }
