891a907a6f43d01144a3938ab072be211a7415d4
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 15:08:09 +0600

    Update HSQLDBGroupKickTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBGroupKickTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBGroupKickTransactionRepository.java
@@ -12,52 +12,57 @@ import java.sql.SQLException;
 
 public class HSQLDBGroupKickTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBGroupKickTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBGroupKickTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT group_id, address, reason, member_reference, admin_reference, join_reference, previous_group_id FROM GroupKickTransactions WHERE signature = ?";
+    @Override
+    public TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT group_id, address, reason, member_reference, admin_reference, join_reference, previous_group_id " +
+                     "FROM GroupKickTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
+            if (resultSet == null || !resultSet.next()) {
+                return null;  // Return null if no result is found
+            }
 
-			int groupId = resultSet.getInt(1);
-			String member = resultSet.getString(2);
-			String reason = resultSet.getString(3);
-			byte[] memberReference = resultSet.getBytes(4);
-			byte[] adminReference = resultSet.getBytes(5);
-			byte[] joinReference = resultSet.getBytes(6);
+            int groupId = resultSet.getInt("group_id");
+            String member = resultSet.getString("address");
+            String reason = resultSet.getString("reason");
+            byte[] memberReference = resultSet.getBytes("member_reference");
+            byte[] adminReference = resultSet.getBytes("admin_reference");
+            byte[] joinReference = resultSet.getBytes("join_reference");
 
-			Integer previousGroupId = resultSet.getInt(7);
-			if (previousGroupId == 0 && resultSet.wasNull())
-				previousGroupId = null;
+            Integer previousGroupId = resultSet.getInt("previous_group_id");
+            if (previousGroupId == 0 && resultSet.wasNull()) {
+                previousGroupId = null;
+            }
 
-			return new GroupKickTransactionData(baseTransactionData, groupId, member, reason, memberReference, adminReference,
-					joinReference, previousGroupId);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch group kick transaction from repository", e);
-		}
-	}
+            return new GroupKickTransactionData(baseTransactionData, groupId, member, reason, memberReference, adminReference,
+                    joinReference, previousGroupId);
+        } catch (SQLException e) {
+            throw new DataException("Unable to fetch group kick transaction from repository", e);
+        }
+    }
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		GroupKickTransactionData groupKickTransactionData = (GroupKickTransactionData) transactionData;
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        GroupKickTransactionData groupKickTransactionData = (GroupKickTransactionData) transactionData;
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("GroupKickTransactions");
+        try (HSQLDBSaver saveHelper = new HSQLDBSaver("GroupKickTransactions")) {
+            saveHelper.bind("signature", groupKickTransactionData.getSignature())
+                      .bind("admin", groupKickTransactionData.getAdminPublicKey())
+                      .bind("group_id", groupKickTransactionData.getGroupId())
+                      .bind("address", groupKickTransactionData.getMember())
+                      .bind("reason", groupKickTransactionData.getReason())
+                      .bind("member_reference", groupKickTransactionData.getMemberReference())
+                      .bind("admin_reference", groupKickTransactionData.getAdminReference())
+                      .bind("join_reference", groupKickTransactionData.getJoinReference())
+                      .bind("previous_group_id", groupKickTransactionData.getPreviousGroupId());
 
-		saveHelper.bind("signature", groupKickTransactionData.getSignature()).bind("admin", groupKickTransactionData.getAdminPublicKey())
-				.bind("group_id", groupKickTransactionData.getGroupId()).bind("address", groupKickTransactionData.getMember())
-				.bind("reason", groupKickTransactionData.getReason()).bind("member_reference", groupKickTransactionData.getMemberReference())
-				.bind("admin_reference", groupKickTransactionData.getAdminReference()).bind("join_reference", groupKickTransactionData.getJoinReference())
-				.bind("previous_group_id", groupKickTransactionData.getPreviousGroupId());
-
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save group kick transaction into repository", e);
-		}
-	}
-
+            saveHelper.execute(this.repository);
+        } catch (SQLException e) {
+            throw new DataException("Unable to save group kick transaction into repository", e);
+        }
+    }
 }
