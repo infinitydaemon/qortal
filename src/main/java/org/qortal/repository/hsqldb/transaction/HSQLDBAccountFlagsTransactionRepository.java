2a5d0988c2f003c5114d9ba717cf23b268403e41
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 12:15:43 +0600

    Update HSQLDBAccountFlagsTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBAccountFlagsTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBAccountFlagsTransactionRepository.java
@@ -12,48 +12,64 @@ import java.sql.SQLException;
 
 public class HSQLDBAccountFlagsTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBAccountFlagsTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBAccountFlagsTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT target, and_mask, or_mask, xor_mask, previous_flags FROM AccountFlagsTransactions WHERE signature = ?";
+    @Override
+    TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT target, and_mask, or_mask, xor_mask, previous_flags FROM AccountFlagsTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = executeQuery(sql, baseTransactionData.getSignature())) {
+            if (resultSet == null) return null;
 
-			String target = resultSet.getString(1);
-			int andMask = resultSet.getInt(2);
-			int orMask = resultSet.getInt(3);
-			int xorMask = resultSet.getInt(4);
+            String target = resultSet.getString(1);
+            int andMask = resultSet.getInt(2);
+            int orMask = resultSet.getInt(3);
+            int xorMask = resultSet.getInt(4);
+            Integer previousFlags = getNullableInteger(resultSet, 5);
 
-			Integer previousFlags = resultSet.getInt(5);
-			if (previousFlags == 0 && resultSet.wasNull())
-				previousFlags = null;
+            return new AccountFlagsTransactionData(baseTransactionData, target, andMask, orMask, xorMask, previousFlags);
+        } catch (SQLException e) {
+            throw new DataException("Unable to fetch account flags transaction from repository", e);
+        }
+    }
 
-			return new AccountFlagsTransactionData(baseTransactionData, target, andMask, orMask, xorMask, previousFlags);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch account flags transaction from repository", e);
-		}
-	}
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        AccountFlagsTransactionData accountFlagsTransactionData = (AccountFlagsTransactionData) transactionData;
+        String tableName = "AccountFlagsTransactions";
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		AccountFlagsTransactionData accountFlagsTransactionData = (AccountFlagsTransactionData) transactionData;
+        HSQLDBSaver saveHelper = new HSQLDBSaver(tableName)
+                .bind("signature", accountFlagsTransactionData.getSignature())
+                .bind("creator", accountFlagsTransactionData.getCreatorPublicKey())
+                .bind("target", accountFlagsTransactionData.getTarget())
+                .bind("and_mask", accountFlagsTransactionData.getAndMask())
+                .bind("or_mask", accountFlagsTransactionData.getOrMask())
+                .bind("xor_mask", accountFlagsTransactionData.getXorMask())
+                .bind("previous_flags", accountFlagsTransactionData.getPreviousFlags());
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("AccountFlagsTransactions");
+        executeSave(saveHelper);
+    }
 
-		saveHelper.bind("signature", accountFlagsTransactionData.getSignature()).bind("creator", accountFlagsTransactionData.getCreatorPublicKey())
-				.bind("target", accountFlagsTransactionData.getTarget()).bind("and_mask", accountFlagsTransactionData.getAndMask())
-				.bind("or_mask", accountFlagsTransactionData.getOrMask()).bind("xor_mask", accountFlagsTransactionData.getXorMask())
-				.bind("previous_flags", accountFlagsTransactionData.getPreviousFlags());
+    private ResultSet executeQuery(String sql, Object... params) throws DataException {
+        try {
+            return this.repository.checkedExecute(sql, params);
+        } catch (SQLException e) {
+            throw new DataException("Error executing query", e);
+        }
+    }
 
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save account flags transaction into repository", e);
-		}
-	}
+    private void executeSave(HSQLDBSaver saveHelper) throws DataException {
+        try {
+            saveHelper.execute(this.repository);
+        } catch (SQLException e) {
+            throw new DataException("Error saving transaction into repository", e);
+        }
+    }
 
+    private Integer getNullableInteger(ResultSet resultSet, int columnIndex) throws SQLException {
+        int value = resultSet.getInt(columnIndex);
+        return (value == 0 && resultSet.wasNull()) ? null : value;
+    }
 }
