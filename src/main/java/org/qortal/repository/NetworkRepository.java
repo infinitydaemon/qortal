3ba479f895941ef045c0cf5b5078f38a84f9c68c
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 15:24:42 +0600

    Update NetworkRepository.java

--- src/main/java/org/qortal/repository/NetworkRepository.java
+++ src/main/java/org/qortal/repository/NetworkRepository.java
@@ -4,15 +4,13 @@ import org.qortal.data.network.PeerData;
 import org.qortal.network.PeerAddress;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NetworkRepository {
 
-	public List<PeerData> getAllPeers() throws DataException;
-
-	public void save(PeerData peerData) throws DataException;
-
-	public int delete(PeerAddress peerAddress) throws DataException;
-
-	public int deleteAllPeers() throws DataException;
-
+    public List<PeerData> getAllPeers() throws DataException;
+    public void save(PeerData peerData) throws DataException;
+    public boolean delete(PeerAddress peerAddress) throws DataException;
+    public int deleteAllPeers() throws DataException;
+    public Optional<PeerData> getPeerByAddress(PeerAddress peerAddress) throws DataException;
 }
