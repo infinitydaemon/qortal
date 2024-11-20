6dc78177ca6543da95a8d36c6d8b9f14ca963c23
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 15:12:22 +0600

    Update HSQLDBPublicizeTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBPublicizeTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBPublicizeTransactionRepository.java
@@ -12,39 +12,43 @@ import java.sql.SQLException;
 
 public class HSQLDBPublicizeTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBPublicizeTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBPublicizeTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT nonce FROM PublicizeTransactions WHERE signature = ?";
+    @Override
+    public TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT nonce FROM PublicizeTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
+            // If no record is found, return null
+            if (!resultSet.next()) {
+                return null;
+            }
 
-			int nonce = resultSet.getInt(1);
+            // Retrieve nonce from result set
+            int nonce = resultSet.getInt("nonce");
 
-			return new PublicizeTransactionData(baseTransactionData, nonce);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch publicize transaction from repository", e);
-		}
-	}
+            // Return the PublicizeTransactionData instance with the retrieved values
+            return new PublicizeTransactionData(baseTransactionData, nonce);
+        } catch (SQLException e) {
+            throw new DataException("Unable to fetch publicize transaction from repository", e);
+        }
+    }
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		PublicizeTransactionData publicizeTransactionData = (PublicizeTransactionData) transactionData;
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        PublicizeTransactionData publicizeTransactionData = (PublicizeTransactionData) transactionData;
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("PublicizeTransactions");
+        try (HSQLDBSaver saveHelper = new HSQLDBSaver("PublicizeTransactions")) {
+            // Bind values to the SQL query
+            saveHelper.bind("signature", publicizeTransactionData.getSignature())
+                      .bind("nonce", publicizeTransactionData.getNonce());
 
-		saveHelper.bind("signature", publicizeTransactionData.getSignature())
-				.bind("nonce", publicizeTransactionData.getNonce());
-
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save publicize transaction into repository", e);
-		}
-	}
-
+            // Execute the insert operation
+            saveHelper.execute(this.repository);
+        } catch (SQLException e) {
+            throw new DataException("Unable to save publicize transaction into repository", e);
+        }
+    }
 }
