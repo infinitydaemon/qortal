ff682ce15dbeb54dac27371b1e35f9a3fcfb252e
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-11-20 12:21:36 +0600

    Update HSQLDBCancelGroupInviteTransactionRepository.java

--- src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBCancelGroupInviteTransactionRepository.java
+++ src/main/java/org/qortal/repository/hsqldb/transaction/HSQLDBCancelGroupInviteTransactionRepository.java
@@ -12,42 +12,56 @@ import java.sql.SQLException;
 
 public class HSQLDBCancelGroupInviteTransactionRepository extends HSQLDBTransactionRepository {
 
-	public HSQLDBCancelGroupInviteTransactionRepository(HSQLDBRepository repository) {
-		this.repository = repository;
-	}
+    public HSQLDBCancelGroupInviteTransactionRepository(HSQLDBRepository repository) {
+        this.repository = repository;
+    }
 
-	TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
-		String sql = "SELECT group_id, invitee, invite_reference FROM CancelGroupInviteTransactions WHERE signature = ?";
+    @Override
+    TransactionData fromBase(BaseTransactionData baseTransactionData) throws DataException {
+        String sql = "SELECT group_id, invitee, invite_reference FROM CancelGroupInviteTransactions WHERE signature = ?";
 
-		try (ResultSet resultSet = this.repository.checkedExecute(sql, baseTransactionData.getSignature())) {
-			if (resultSet == null)
-				return null;
+        try (ResultSet resultSet = executeQuery(sql, baseTransactionData.getSignature())) {
+            if (resultSet == null) return null;
 
-			int groupId = resultSet.getInt(1);
-			String invitee = resultSet.getString(2);
-			byte[] inviteReference = resultSet.getBytes(3);
+            int groupId = resultSet.getInt(1);
+            String invitee = resultSet.getString(2);
+            byte[] inviteReference = resultSet.getBytes(3);
 
-			return new CancelGroupInviteTransactionData(baseTransactionData, groupId, invitee, inviteReference);
-		} catch (SQLException e) {
-			throw new DataException("Unable to fetch cancel group invite transaction from repository", e);
-		}
-	}
+            return new CancelGroupInviteTransactionData(baseTransactionData, groupId, invitee, inviteReference);
+        } catch (SQLException e) {
+            throw new DataException("Unable to fetch cancel group invite transaction from repository", e);
+        }
+    }
 
-	@Override
-	public void save(TransactionData transactionData) throws DataException {
-		CancelGroupInviteTransactionData cancelGroupInviteTransactionData = (CancelGroupInviteTransactionData) transactionData;
+    @Override
+    public void save(TransactionData transactionData) throws DataException {
+        CancelGroupInviteTransactionData cancelGroupInviteTransactionData = (CancelGroupInviteTransactionData) transactionData;
 
-		HSQLDBSaver saveHelper = new HSQLDBSaver("CancelGroupInviteTransactions");
+        HSQLDBSaver saveHelper = new HSQLDBSaver("CancelGroupInviteTransactions")
+                .bind("signature", cancelGroupInviteTransactionData.getSignature())
+                .bind("admin", cancelGroupInviteTransactionData.getAdminPublicKey())
+                .bind("group_id", cancelGroupInviteTransactionData.getGroupId())
+                .bind("invitee", cancelGroupInviteTransactionData.getInvitee())
+                .bind("invite_reference", cancelGroupInviteTransactionData.getInviteReference());
 
-		saveHelper.bind("signature", cancelGroupInviteTransactionData.getSignature()).bind("admin", cancelGroupInviteTransactionData.getAdminPublicKey())
-				.bind("group_id", cancelGroupInviteTransactionData.getGroupId()).bind("invitee", cancelGroupInviteTransactionData.getInvitee())
-				.bind("invite_reference", cancelGroupInviteTransactionData.getInviteReference());
+        executeSave(saveHelper);
+    }
 
-		try {
-			saveHelper.execute(this.repository);
-		} catch (SQLException e) {
-			throw new DataException("Unable to save cancel group invite transaction into repository", e);
-		}
-	}
+    // Helper method to execute SQL queries
+    private ResultSet executeQuery(String sql, Object... params) throws DataException {
+        try {
+            return repository.checkedExecute(sql, params);
+        } catch (SQLException e) {
+            throw new DataException("Error executing query", e);
+        }
+    }
 
+    // Helper method to execute save operations
+    private void executeSave(HSQLDBSaver saveHelper) throws DataException {
+        try {
+            saveHelper.execute(repository);
+        } catch (SQLException e) {
+            throw new DataException("Error saving transaction into repository", e);
+        }
+    }
 }
